0a16014dd078942044b485049a846b55010aeb3e
Gustav Kotze <edgegustavkotze@gmail.com>
2021-03-29 14:55:24 +0200

    to const

--- script/burgerToCross.js
+++ script/burgerToCross.js
@@ -1,10 +1,10 @@
 
   function rectChange() {
-    var b = document.getElementById("toggle");
-    var cross = document.getElementById("crossMain");
-    var burger = document.getElementById("burgerMain");
-    var cross1 = document.getElementById("cross1");
-    var cross2 = document.getElementById("cross2");
+    const b = document.getElementById("toggle");
+    const cross = document.getElementById("crossMain");
+    const burger = document.getElementById("burgerMain");
+    const cross1 = document.getElementById("cross1");
+    const cross2 = document.getElementById("cross2");
     if( toggle.checked == 0){
       cross.style.display = "block";
       burger.style.display = "none";
@@ -19,11 +19,11 @@
     
   }
   function closeIt() {
-    var toggle = document.getElementById("toggle").checked = 1;
-    var cross = document.getElementById("crossMain");
-    var burger = document.getElementById("burgerMain");
-    var cross1 = document.getElementById("cross1");
-    var cross2 = document.getElementById("cross2");
+    const toggle = document.getElementById("toggle").checked = 1;
+    const cross = document.getElementById("crossMain");
+    const burger = document.getElementById("burgerMain");
+    const cross1 = document.getElementById("cross1");
+    const cross2 = document.getElementById("cross2");
     cross.style.display = "none";
     burger.style.display = "block";
     cross1.classList.remove("cross1");
